5d1a2b971d7919b3672a10c4a92eee9f16dfe404
Caleb Nasio <caleb.nasio@kyosk.app>
2024-02-08 12:28:39 +0300

    Refactored code and ignored files under dist folder

--- index.js
+++ index.js
@@ -1,64 +1,49 @@
 const fs = require('fs');
 const path = require('path');
-const {execSync} = require('child_process')
-const swagger = require ('./swagger.json');
+const { execSync } = require('child_process');
+const swagger = require('./swagger.json');
 
-const foldersArray = []
+const foldersArray = [];
 
-// Read all the folders in 'docs' for all teams and create an array of the names.
 const readAllDocFolders = (docsFolderPath) => {
     const folders = fs.readdirSync(docsFolderPath);
-    folders.forEach(folder => {
+    folders.forEach((folder) => {
         const folderPath = path.join(docsFolderPath, folder);
         const stats = fs.statSync(folderPath);
         if (stats.isDirectory()) {
             foldersArray.push(folder);
         }
     });
-    return createTagsInJsonFiles();
-}
+    createTagsInJsonFiles();
+};
 
-// Check if the folder exists
-const doesDirectoryExist = (path) => {
+const doesDirectoryExist = (directoryPath) => {
     try {
-        return fs.statSync(path).isDirectory();
+        return fs.statSync(directoryPath).isDirectory();
     } catch (error) {
         if (error.code === 'ENOENT') {
             return false; // Path does not exist
-        } else {
-            throw error; // Other error (e.g., permission denied)
         }
+        throw error; // Other error (e.g., permission denied)
     }
-}
+};
 
-// Loop through each folder and it's tags create the JSON doc named as the
-// folders structure from the template.
 const createTagsInJsonFiles = () => {
-    // Create deep clone of the JSON template
     const templateCopy = JSON.parse(JSON.stringify(swagger));
-    let newTags = [];
+    const newTags = [];
     const newJSONFilesArray = [];
 
-    // Get each folder and its embedded tags folder, get the tags JSON files in the
-    // tags folder & inject them into the JSON document template.
-    foldersArray.map(folder => {
+    foldersArray.forEach((folder) => {
         const directoryPath = `./docs/${folder}/tags`;
 
         if (!doesDirectoryExist(directoryPath)) return;
 
-        // Read the contents of the tags folder
         const files = fs.readdirSync(directoryPath);
-        // Filter out JSON files
-        const jsonFiles = files.filter(file => {
-            if (file === 'tagGroups.json') return;
-            return path.extname(file).toLowerCase() === '.json';
-        });
+        const jsonFiles = files.filter((file) => path.extname(file).toLowerCase() === '.json' && file !== 'tagGroups.json');
 
-        // Read the content of each tag JSON file
-        jsonFiles.forEach(jsonFile => {
+        jsonFiles.forEach((jsonFile) => {
             const filePath = path.join(directoryPath, jsonFile);
             const fileContent = fs.readFileSync(filePath, 'utf8');
-            // Combine all the JSON files to a single Array
             try {
                 const jsonData = JSON.parse(fileContent);
                 newTags.push(jsonData);
@@ -67,8 +52,7 @@ const createTagsInJsonFiles = () => {
             }
         });
 
-        const infoContent = getDocumentationInfo(`./docs/${folder}`)
-
+        const infoContent = getDocumentationInfo(`./docs/${folder}`);
         const tagGroupPath = `${directoryPath}/tagGroups.json`;
         let tagGroupContent = [];
 
@@ -78,80 +62,72 @@ const createTagsInJsonFiles = () => {
             console.error(`Error parsing JSON: ${error.message}. Using default value.`);
         }
 
-        const {info} = templateCopy
+        const { info } = templateCopy;
         const newJSON = {
             ...templateCopy,
-            info:{
+            info: {
                 ...info,
-                ...infoContent
+                ...infoContent,
             },
             service: folder,
             tags: newTags,
-            'x-tagGroups': tagGroupContent
-        }
+            'x-tagGroups': tagGroupContent,
+        };
         newJSONFilesArray.push(newJSON);
-    })
-
-    createNewSwaggerFiles(newJSONFilesArray);
-}
-
-// Get the information about the project documentation.
-const getDocumentationInfo = (directoryPath) => {
-    // Read the contents of the info folder
-    const infoFilePath = `${directoryPath}/info.json`;
-    return JSON.parse(fs.readFileSync(infoFilePath, 'utf8'));
-}
-// Create new swagger files for each tags group data and paths structure in document.
-const createNewSwaggerFiles = (JSONFilesArray) => {
-    JSONFilesArray.map(jsonFile => {
-        readPathJsonFiles(jsonFile);
-    })
-}
-
-// Fetch each path in the structure and created an appended JSON .
-const readPathJsonFiles = (jsonData) => {
-    const {paths} = jsonData;
-    let newPath = {};
-    const docsDirectory = `./docs/${jsonData.service}`;
-
-    // Read the contents of the folder
-    const files = fs.readdirSync(docsDirectory);
-
-    // Filter out endpoint JSON path files
-    const jsonFiles = files.filter(file => {
-        if (file === 'info.json') return;
-        return path.extname(file).toLowerCase() === '.json'
     });
 
-    // Read the content of each JSON file
-    jsonFiles.forEach(jsonFile => {
+    createNewSwaggerFiles(newJSONFilesArray);
+};
+
+const getDocumentationInfo = (directoryPath) => {
+    const infoFilePath = `${directoryPath}/info.json`;
+    return doesFileExist(infoFilePath) ? JSON.parse(fs.readFileSync(infoFilePath, 'utf8')) : {};
+};
+
+const doesFileExist = (filePath) => {
+    try {
+        return fs.statSync(filePath).isFile();
+    } catch (error) {
+        return false;
+    }
+};
+
+const createNewSwaggerFiles = (JSONFilesArray) => {
+    JSONFilesArray.forEach((jsonFile) => {
+        readPathJsonFiles(jsonFile);
+    });
+};
+
+const readPathJsonFiles = (jsonData) => {
+    const { paths } = jsonData;
+    let newPath = {};
+    const docsDirectory = `./docs/${jsonData.service}`;
+    const files = fs.readdirSync(docsDirectory);
+
+    const jsonFiles = files.filter((file) => path.extname(file).toLowerCase() === '.json' && file !== 'info.json');
+
+    jsonFiles.forEach((jsonFile) => {
         const filePath = path.join(docsDirectory, jsonFile);
         const fileContent = fs.readFileSync(filePath, 'utf8');
-        // Combine all the JSON files to a single JSON
         try {
             const jsonData = JSON.parse(fileContent);
-            newPath = {...paths, ...newPath, ...jsonData};
+            newPath = { ...paths, ...newPath, ...jsonData };
         } catch (error) {
             console.error(`Error parsing JSON file ${jsonFile}:`, error.message);
         }
     });
-    createJSONFile({...jsonData, paths: newPath});
-}
+    createJSONFile({ ...jsonData, paths: newPath });
+};
 
-// Generate resulting final openAPI JSON file
 const createJSONFile = (jsonData) => {
     const data = JSON.stringify(jsonData, null, 2);
     fs.writeFileSync(`dist/${jsonData.service}.json`, data);
+    createHTMLServiceFile(jsonData.service);
+};
 
-    createHTMLServiceFile(jsonData.service)
-}
-
-// Generate resulting final HTML file
 const createHTMLServiceFile = (service) => {
     execSync(`redocly build-docs dist/${service}.json --output=dist/${service}.html`);
-}
+};
 
 const folderPath = './docs';
 readAllDocFolders(folderPath);
-
-
